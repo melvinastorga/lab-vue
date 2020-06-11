b97cc4118173879700a692166fd7c8a0b0206e72
melvinastorga <31803448+melvinastorga@users.noreply.github.com>
2020-06-10 22:57:29 -0600

    api student

---
+++ WebAPILabI2020/WebAPILabI2020/WebAPILabI2020/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using WebAPILabI2020.Converters;
+
+namespace WebAPILabI2020.Models
+{
+    public partial class Student
+    {
+        [JsonConverter(typeof(IntToStringConverter))]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        [JsonConverter(typeof(IntToStringConverter))]
+        public int Age { get; set; }
+        [JsonConverter(typeof(IntToStringConverter))]
+        public int Nationality { get; set; }
+        [JsonConverter(typeof(IntToStringConverter))]
+        public int Major { get; set; }
+        public string NationalityName { get; set; }
+        public string MajorName { get; set; }
+
+
+        public virtual Major MajorNavigation { get; set; }
+        public virtual Nationality NationalityNavigation { get; set; }
+    }
+}
---
+++ WebAPILabI2020/WebAPILabI2020/WebAPILabI2020/Models/_2020_lenguajes_B70785_2Context.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAPILabI2020.Models
+{
+    public partial class _2020_lenguajes_B70785_2Context : DbContext
+    {
+        public _2020_lenguajes_B70785_2Context()
+        {
+        }
+
+        public _2020_lenguajes_B70785_2Context(DbContextOptions<_2020_lenguajes_B70785_2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Major> Major { get; set; }
+        public virtual DbSet<Nationality> Nationality { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=163.178.173.148;Initial Catalog=2020_lenguajes_B70785_2;User ID=estudiantesrp;Password=estudiantesrp");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Major>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Nationality>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.MajorNavigation)
+                    .WithMany(p => p.Student)
+                    .HasForeignKey(d => d.Major)
+                    .HasConstraintName("Major_fk");
+
+                entity.HasOne(d => d.NationalityNavigation)
+                    .WithMany(p => p.Student)
+                    .HasForeignKey(d => d.Nationality)
+                    .HasConstraintName("Nationality_fk");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
